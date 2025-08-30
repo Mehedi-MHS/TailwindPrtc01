9b66c41d571fbe482d0b495a212460528a4a14a0
Mehedi-MHS <mehedishuvo685638@gmail.com>
2025-08-30 19:05:17 +0600

    added header using gemini

---
+++ my-app/src/App.jsx
@@ -0,0 +1,12 @@
+import Header from "./components/Header"
+
+export default function App() {
+  return (
+    <div className="w-full min-h-screen">
+      <Header />
+      <div className="p-8">
+        <p className="text-xl">Hello World!</p>
+      </div>
+    </div>
+  )
+}
